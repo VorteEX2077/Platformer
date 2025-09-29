0a6ad5535a35206a69b16358ecaa8c35dbd26d2d
VorteEX2077 <antiprinciplev@gmail.com>
2025-09-29 18:57:47 +0100

    update

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,11 +8,11 @@ import java.util.HashSet;
 
 public class GamePanel extends JPanel implements KeyListener {
 
+    private static final int JUMP_STRENGTH = 40;
     int playerWidth = 50;
     int playerHeight = 100;
     int playerX = 370;
     int playerY = 400;
-    int platformY;
     DataLoader dataLoader;
     boolean isJumping;
     int velocityY = -22;
@@ -25,7 +25,8 @@ public class GamePanel extends JPanel implements KeyListener {
         setFocusable(true);
         addKeyListener(this);
         requestFocus();
-        dataLoader = new DataLoader();
+        dataLoader = new DataLoader(); // when the new dataloader is ran it calls the constructor and loads
+        // all the images
     }
 
     @Override
@@ -69,43 +70,70 @@ public class GamePanel extends JPanel implements KeyListener {
             }
         }
         if (keyEvent.contains(KeyEvent.VK_W) || keyEvent.contains(KeyEvent.VK_SPACE)) {
-            if (!isJumping) {
+            if (onPlatform && !isJumping) {
                 isJumping = true;
+                onPlatform = false;
+                velocityY = -JUMP_STRENGTH;  // <-- the missing upward impulse
             }
             // TODO: move the background
         }
     }
 
     public void update() {
-        /* This method is called 100 times every sec  */
-        velocityY += GRAVITY;
-        playerY += velocityY;
+        // Apply gravity if in the air
+        if (!onPlatform) {
+            velocityY += GRAVITY;
+            playerY += velocityY;
+        }
 
-        if(!isPlayerOnPlatform() == null){
-            playerY = isPlayerOnPlatform() - rectH; // Sit on top
+        Image imgObj = isPlayerOnPlatform();
+
+        if (imgObj != null && velocityY >= 0) { // land only when falling or resting
+            playerY = imgObj.y - playerHeight;  // <-- use playerHeight, not image height
             velocityY = 0;
             onPlatform = true;
             isJumping = false;
-
-        } else if (playerY + playerHeight >= 800) {
         }
-        else {
-
+        // Floor collision
+        else if (playerY + playerHeight >= 800) {
+            playerY = 800 - playerHeight;
+            velocityY = 0;
+            onPlatform = true;
+            isJumping = false;
+        } else {
+            onPlatform = false;
         }
-
     }
 
+
     private Image isPlayerOnPlatform() {
-        Image[] allImagesArray = dataLoader.getImages();
+       Image[] allImagesArray = dataLoader.getImages();
+        // Player's bounding box
+        int playerBottom = playerY + playerHeight;
+        int playerLeft = playerX;
+        int playerRight = playerX + playerWidth;
 
         for (Image img : allImagesArray) {
-            if(playerY + playerHeight >= img.y && playerX >= img.x && playerX <= img.x +
-                    img.getImage().getWidth() && img.type.equals("platform")) {
+            if (!"platform".equals(img.type)) continue;
+
+            int platLeft = img.x;
+            int platRight = img.x + img.getImage().getWidth();
+            int platTop = img.y;
+
+            boolean horizontallyOverlapping = playerRight > platLeft && playerLeft < platRight;
+
+            // Consider as "standing" if player's bottom is at or just above the platform top
+            // and the player is moving downward (velocityY >= 0).
+            boolean touchingTop =
+                    playerBottom >= platTop && playerBottom <= platTop + Math.max(2, Math.abs(velocityY) + 2);
+
+            if (horizontallyOverlapping && touchingTop) {
                 return img;
             }
         }
         return null;
     }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
